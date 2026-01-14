d77c2ab3f5482574139940d49bc9840c178363b0
Kirubel Eshetu Tefera <Kirubelwinner@gmail.com>
2026-01-14 16:21:34 +0300

    wrote 26 lines of js

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 console.log("QuoLand has started!");
+
+const state = {
+    currentBiblicalIndex: 0,
+    currentQuoteIndex: 0,
+    viewedBiblicalQuotes: new Set(),
+    viewedQuotes: new Set(),
+    biblicalQuoteHistory: [],
+    quoteHistory: [],
+    isBibleMode: true,
+    currentBiblicalSection: "oldTestament",
+    currentInspirationalCategory: "inspirational",
+    totalBiblicalQuotesViewed: 0,
+    totalInspirationalQuotesViewed: 0,
+
+    biblicalSequentialIndex: {
+        oldTestament: 0,
+        newTestament: 0,
+        psalms: 0
+    },
+    inspirationalSequentialIndex: {
+        inspirational: 0,
+        strengthening: 0,
+        focus: 0
+    }
+};
+
